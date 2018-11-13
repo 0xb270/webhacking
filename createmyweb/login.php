d2b3348470687cfc277b89fcc170a37b363a7afb
jaeyong <ggjae_@daum.net>
2018-11-14 00:55:07 +0900

    new files

---
+++ createmyweb/login.php
@@ -0,0 +1,10 @@
+<html>
+<body>
+        <form action="login_process.php" method="POST">
+        ID : <input type="text" name="id">
+        <br>
+        PW : <input type="password" name="pw">
+        <input type="submit">
+	</form>
+</body>
+</html>
